6be2a38923c84fd83787490d3e13e81b6885a19e
DarkFeed2005 <kalanayasassrisathruwan123@gmail.com>
2025-06-17 23:45:55 +0530

    script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 window.addEventListener("load", function() {
         setTimeout(function() {
             document.body.classList.add("loaded");
-        }, 550); // ⏱️ Delay in milliseconds (2000 = 2 seconds)
+        }, 1000); // ⏱️ Delay in milliseconds (2000 = 2 seconds)
     });
